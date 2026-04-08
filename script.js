4fb6f739e6e80996456691debe7bf576997ed63e
Hugues <huguesbaya10@gmail.com>
2026-04-08 13:59:25 +0100

    Mis à du script

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 function activerMagie() {
-            // On change la bordure de la carte e>
-            document.getElementById('maCarte').st>
+    document.getElementById('maCarte').style.borderColor = '#2ecc71';
+    document.getElementById('statut').innerHTML = 'Super Puissance Activée ⚡';
+    document.getElementById('statut').style.color = '#2ecc71';
+    alert("Magie activée !");
+}
 
-            // On modifie le texte du statut
-            document.getElementById('statut').inn>
-            document.getElementById('statut').sty>
-            document.getElementById('horloge').in>
-
-
-            // On affiche une fenêtre pop-up
-            alert("Bienvenue dans le monde de Jav>
-        }
-        function changerMode() {
-    // On récupère le corps de la page (le body)
+function changerMode() {
     let corps = document.body;
-
-    // Si la couleur est bleu foncé, on passe au >
-    if (corps.style.backgroundColor === "rgb(44, >
+    if (corps.style.backgroundColor === "rgb(44, 62, 80)") {
         corps.style.backgroundColor = "#ecf0f1";
         corps.style.color = "#2c3e50";
     } else {
