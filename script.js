dcdaeb4c0cbfffc1389a656c50771b9a339197c2
Hugues <huguesbaya10@gmail.com>
2026-04-08 14:26:05 +0100

    Modifications

--- script.js
+++ script.js
@@ -4,14 +4,3 @@ function activerMagie() {
     document.getElementById('statut').style.color = '#2ecc71';
     alert("Magie activée !");
 }
-
-function changerMode() {
-    let corps = document.body;
-    if (corps.style.backgroundColor === "rgb(44, 62, 80)") {
-        corps.style.backgroundColor = "#ecf0f1";
-        corps.style.color = "#2c3e50";
-    } else {
-        corps.style.backgroundColor = "#2c3e50";
-        corps.style.color = "white";
-    }
-}
