5e729b0ea3ba05e6c0e5bf5aa156275f34526be8
Hugues <huguesbaya10@gmail.com>
2026-04-08 13:39:59 +0100

    Migration vers une architecture propre HTML/CSS/JS

---
+++ script.js
@@ -0,0 +1,26 @@
+function activerMagie() {
+            // On change la bordure de la carte e>
+            document.getElementById('maCarte').st>
+
+            // On modifie le texte du statut
+            document.getElementById('statut').inn>
+            document.getElementById('statut').sty>
+            document.getElementById('horloge').in>
+
+
+            // On affiche une fenêtre pop-up
+            alert("Bienvenue dans le monde de Jav>
+        }
+        function changerMode() {
+    // On récupère le corps de la page (le body)
+    let corps = document.body;
+
+    // Si la couleur est bleu foncé, on passe au >
+    if (corps.style.backgroundColor === "rgb(44, >
+        corps.style.backgroundColor = "#ecf0f1";
+        corps.style.color = "#2c3e50";
+    } else {
+        corps.style.backgroundColor = "#2c3e50";
+        corps.style.color = "white";
+    }
+}
